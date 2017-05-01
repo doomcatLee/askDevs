58e840c388ac78e57e5ea682eb5ec029f1e72848
Alex Lee <doomcat9167@gmail.com>
2017-05-01 14:05:05 -0700

    individually display questions and answers in hashed out id

---
+++ app/components/new-answer.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    saveAnswer() {
+     var params = {
+       name: this.get('name'),
+       content: this.get('content'),
+       rating: this.get('rating')
+     };
+     this.set('addNewAnswer', false);
+     this.sendAction('saveAnswer', params);
+   }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,28 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question'),
+      answers: this.store.findAll('answer')
+    });
   },
-
   actions: {
     saveQuestion3(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
     },
-
-    update(rental, params) {
-    Object.keys(params).forEach(function(key) {
-      if(params[key]!==undefined) {
-        rental.set(key,params[key]);
-      }
-    });
-    rental.save();
-    this.transitionTo('index');
-  },
-
-    destroyQuestion(question) {
-      question.destroyRecord();
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
       this.transitionTo('index');
     }
   }
